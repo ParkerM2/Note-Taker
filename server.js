c253f34dac783709cf061a62c6c44a4c7142d0dd
Parker Manning <parkerlmanning@hotmail.com>
2020-10-08 03:03:04 -0500

    save

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const logger = require('./02-Homework/Develop/public/assets/js/logger');
-const { response, json } = require('express');
 const uuid = require('uuid')
-let noteArr = [];
+const noteArr = require('./02-Homework/Develop/db/db.json')
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
@@ -13,9 +12,9 @@ app.use(express.static(path.join(__dirname + 'public', 'index.html')));
 app.use(express.json());
 // app.use(logger);
 // route for index file..
-app.get('*', (req, res) => { res.sendFile(path.join(__dirname + './develop/public', 'index.html')) }); // <- the index.js file throws a jquery error for the method forEach() when this is active
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname + './develop/public/index.html')) }); // <- the index.js file throws a jquery error for the method forEach() when this is active
 // route for GET Notes
-app.get('/notes', (req, res) => { res.sendFile(path.join(__dirname + '/public', 'notes.html')) });
+app.get('/notes', (req, res) => { res.sendFile(path.join(__dirname + '/public/notes.html')) });
 
 // GETS Notes from db.JSON
 app.get('/api/notes', (req, res) => {
