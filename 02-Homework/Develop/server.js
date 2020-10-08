e49cfc6f049c2dddca6930b5442198c859dcc8c1
Parker Manning <parkerlmanning@hotmail.com>
2020-10-08 01:53:55 -0500

    added randomized id, finished most of routes, starting on saving by title on the left side of screen

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -4,58 +4,43 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const fs = require('fs');
 const logger = require('./public/assets/js/logger');
-
-
-
-
+const { response, json } = require('express');
+const { isBuffer } = require('util');
+const uuid = require('uuid')
+let noteArr = [];
 // middleware
 // app.use(logger);
-app.use(express.static('./public/assets/index.html'))
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
-
-
-//route for home page?
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public', 'index.html'))
-})
-
 // route for GET Notes
-app.get('/notes.html', (req, res) => {
-    // console.log(res);
-    res.sendFile(path.join(__dirname + '/public', 'notes.html'))
-    // res.json(notes)
-});
-
+app.get('/notes', (req, res) => {res.sendFile(path.join(__dirname + '/public', 'notes.html'))});
 // GETS Notes from db.JSON
-// RETURNS as JSON format from /api/notes
 app.get('/api/notes', (req, res) => {
-    // console.log(res);
-    fs.readFile(__dirname + '/db/db.json', (err, data) => {
-        if (err) throw err;
-        const note = JSON.parse(data)
-        return res.json(note)
-    })
-})
-
-app.post('/api/notes', function (req, res) {
-    fs.writeFile(__dirname + '/db/db.json', function (err, data) {
-        if (err) throw err;
-        const newNote = req.body;
-        console.log(`new note ${newNote}`);
-        // create array to push new notes to
-        let notes = [];
-        // give each note an id
-        let id = [];
-
-
-    })
+    res.json(noteArr);
 });
 
+// first read the db.json file 
+app.post('/api/notes', function (req, res) {
+    const newNote = {
+        id: uuid.v4(),
+        title: req.body.title,
+        text: req.body.text
+    };
+    noteArr.push(newNote);
+    res.json(noteArr);
+    console.table(noteArr);
+});
 
-
-
-
+// deleting a note
+app.delete("/api/notes/:id", function(req, res){
+    let noteId = req.params.id;
+    for (let i = 0; i < notesArr.length; i++){
+        if (noteId === notesArr[i].id){
+            notesArr.splice(i, 1);
+            res.json(notesArr);
+        };
+    };
+});
 app.listen(PORT, () => console.log(`Server Started on PORT ${PORT}`));
 
